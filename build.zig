ffd3a1251ae6ee21ec1eaa15c28b6b0ab656148e
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-05-07 10:14:16 +0100

    only download the submodule you intent to use

--- build.zig
+++ build.zig
@@ -222,30 +222,27 @@ fn download_submodules(b: *std.Build, cpu_count: []const u8, webgpu_backend: Web
             "external/imgui",
             "external/dear_bindings",
             "external/sdl3webgpu",
-            if (webgpu_backend == .@"wgpu-native") "external/wgpu-native" else " ",
         },
         b.allocator,
     );
 
     _ = recursive.spawnAndWait() catch @panic("Couldn't download git submodules...");
 
-    if (webgpu_backend == .dawn) {
-        var dawn_submodule = std.process.Child.init(
-            &.{
-                "git",
-                "submodule",
-                "update",
-                "--init",
-                "--recommend-shallow",
-                "-j",
-                cpu_count,
-                "external/dawn",
-            },
-            b.allocator,
-        );
+    var webgpu_backend_cmd = std.process.Child.init(
+        &.{
+            "git",
+            "submodule",
+            "update",
+            "--init",
+            "--recommend-shallow",
+            "-j",
+            cpu_count,
+            if (webgpu_backend == .dawn) "external/dawn" else "external/wgpu-native",
+        },
+        b.allocator,
+    );
 
-        _ = dawn_submodule.spawnAndWait() catch @panic("Couldn't download git submodules...");
-    }
+    _ = webgpu_backend_cmd.spawnAndWait() catch @panic("Couldn't download git submodules...");
 }
 
 fn make_deps(b: *std.Build, exe: *std.Build.Step.Compile, optimize: std.builtin.OptimizeMode) void {
